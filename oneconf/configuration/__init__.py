2367fe0ba59e4adc712622f61af9f0b4db2d00db
Tomas Korbar <tkorbar@redhat.com>
2019-08-30 16:36:14 +0200

    Add ConfigurationManager

--- oneconf/configuration/__init__.py
+++ oneconf/configuration/__init__.py
@@ -1,3 +1,4 @@
 """Oneconf's modules for work with configurations"""
 from .configuration_reader import ConfigurationReader
 from .configuration_object import ConfigurationObject
+from .configuration_manager import ConfigurationManager
---
+++ oneconf/configuration/configuration_manager.py
@@ -0,0 +1,35 @@
+""" Module containing ConfigurationManager class
+"""
+
+from oneconf.configuration import ConfigurationObject
+
+class ConfigurationManager:
+    """Class containing methods for work with oneconf's configuration objects
+    """
+    def __init__(self):
+        pass
+
+    def merge_conf_objects(self, objects, priority=0):
+        """Merge configuration objects and return the unified one
+
+        Objects are merged with paying attention to priority so object with
+        higher priority will override value of option defined in objects with
+        lower priority
+
+        Positional arguments:
+        objects -- list of configuration objects to be merged
+
+        Keyword arguments:
+        priority -- priority for unified configuration
+        """
+        if len(objects) == 1:
+            return objects[0]
+
+        objects.sort(key=lambda obj: obj.priority)
+
+        conf_dictionaries = [obj.configuration for obj in objects]
+        final_dict = {}
+        for conf_dict in conf_dictionaries:
+            final_dict.update(conf_dict)
+
+        return ConfigurationObject(priority, conf_dict=final_dict)
